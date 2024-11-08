1b5d4903d33cb9e17ba746427aefba9db6204ddb
Dounder <dr.glasdou@gmail.com>
2024-11-08 08:18:36 -0600

    Refactor application modules to integrate Prisma as a separate module and add Redis configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
-  providers: [PrismaService],
+  imports: [PrismaModule, AuthModule, UserModule],
+  providers: [],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AuthModule, AuthService } from 'src/auth';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthModule } from 'src/auth';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { RedisModule } from 'src/redis/redis.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService, AuthService],
-  imports: [RedisModule, AuthModule],
+  providers: [UserService],
+  imports: [RedisModule, AuthModule, PrismaModule],
 })
 export class UserModule {}
